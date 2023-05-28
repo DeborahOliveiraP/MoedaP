79eae897aa2f8a1e74dda283f3abbebcdb5f8d2a
Deborah Oliveira Picado <deborah.oliveirapicado@gmail.com>
2023-05-28 11:26:43 -0300

    Delete moeda directory

--- moeda/src/main/java/com/cambio/moeda/service/ColetarPaisImplement.java
+++
@@ -1,7 +0,0 @@
-package com.cambio.moeda.service;
-
-import com.cambio.moeda.DTO.PairDTO;
-
-public interface ColetarPaisImplement {
-	PairDTO ConsultaPaiSS(String pais); 
-}
--- moeda/src/main/java/com/cambio/moeda/utils/Constantes.java
+++
@@ -1,7 +0,0 @@
-package com.cambio.moeda.utils;
-
-public class Constantes {
-	
-	public static final String BASE_URL = "https://v6.exchangerate-api.com/v6/10b2b3b686810247e258196f/";
-
-}
